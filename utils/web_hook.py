5be8c67bfac38db25870ef97da0ec2af158f5e60
Jessica Orihuela Rojas <Jessica.Orihuela@jalasoft.com>
2025-06-30 20:23:58 -0400

    Update web_hook

--- utils/web_hook.py
+++ utils/web_hook.py
@@ -3,8 +3,8 @@ import pymsteams
 from config.config import web_hook
 
 teams_message = pymsteams.connectorcard(web_hook)
-
-with open("../reports/markdown/md_report.md") as f:
+teams_message.title("Jessica Orihuela - Pytest Project Report")
+with open("reports/markdown/md-report.md") as f:
     report = f.read()
 teams_message.text(report)
 teams_message.send()
